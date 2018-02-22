81190134e893b40fd55dcf86537f371e5ba4a63c
ryym <ryym.64@gmail.com>
2018-02-22 21:16:17 +0900

    Make Message enum to shut down gracefully

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 use std::thread;
 use std::sync::{mpsc, Arc, Mutex};
 
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 impl ThreadPool {
@@ -32,18 +37,21 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
-        self.sender.send(Box::new(f)).unwrap();
+        let job = Box::new(f);
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
         for worker in &mut self.workers {
             println!("Shutting down woerker {}", worker.id);
-
-            // XXX: Option を使って Worker#thread に None を
-            // 残すようにすればコンパイルは通るが、そもそも Worker は
-            // loop で永遠に関数を抜けないので、 join できない。
             if let Some(thread) = worker.thread.take() {
                 thread.join().unwrap();
             }
@@ -57,12 +65,20 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {} got a job; executing.", id);
-            job.call_box();
-            println!("Worker {} done.", id);
+            let message = receiver.lock().unwrap().recv().unwrap();
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job.call_box();
+                    println!("Worker {} done.", id);
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            }
         });
 
         Worker {
