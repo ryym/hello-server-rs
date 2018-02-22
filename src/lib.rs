c9ebc72b1affed4019961f8727a03e6ff02cdcab
ryym <ryym.64@gmail.com>
2018-02-22 14:29:44 +0900

    Store threads in vector?

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
-    pub fn new(size: u32) -> ThreadPool {
-        ThreadPool
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+        for _ in 0..size {
+            // TODO: Create some threads.
+        }
+
+        ThreadPool { threads }
     }
 
     pub fn execute<F>(&self, f: F)
