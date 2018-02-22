c1aafcc3b1b9a8a490792e9bdf9287e2f1455d32
ryym <ryym.64@gmail.com>
2018-02-22 14:18:55 +0900

    Create ThreadPool interface

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,22 @@
+extern crate hello;
+
 use std::io::prelude::*;
 use std::fs::File;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
 
+use hello::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
